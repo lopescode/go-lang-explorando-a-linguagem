26838868e705bba0c7bb6a5bd7c008a98b70270a
LOPES_PC\leona <leonardolopesb97@hotmail.com>
2022-08-17 18:26:46 -0300

    Fundamentos

---
+++ fundamentos/naoternario/naoternario.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Não tem operador ternário
+func obterResultado(nota float64) string {
+	// Não existe isso em GO!
+	// return nota>= 6 ? "Aprovado" : "Reprovado"
+
+	if nota >= 6 {
+		return "Aprovado"
+	}
+
+	return "Reprovado"
+}
+
+func main() {
+	fmt.Println(obterResultado(6.2))
+}
