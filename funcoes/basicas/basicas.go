2f5d37963dc521d0581162b0eb7fd561090621fc
LOPES_PC\leona <leonardolopesb97@hotmail.com>
2022-08-18 14:22:38 -0300

    Funções

---
+++ funcoes/basicas/basicas.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func f1() {
+	fmt.Println("F1")
+}
+
+func f2(p1 string, p2 string) {
+	fmt.Printf("F2: %s %s\n", p1, p2)
+}
+
+func f3() string {
+	return "F3"
+}
+
+func f4(p1, p2 string) string {
+	return fmt.Sprintf("F4: %s %s\n", p1, p2)
+}
+
+func f5() (string, string) {
+	return "Retorno 1", "Retorno2"
+}
+
+func main() {
+	f1()
+	f2("Param1", "Param2")
+	r3, r4 := f3(), f4("Param1", "Param2")
+	fmt.Println(r3)
+	fmt.Println(r4)
+
+	// r51, _ := f5()
+	// fmt.Println("F51:", r51)
+
+	// _, r52 := f5()
+	// fmt.Println("F52:", r52)
+
+	r51, r52 := f5()
+	fmt.Printf("F51: %v, F52: %v", r51, r52)
+}
